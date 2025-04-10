013c6c61f11a738cff3d8e7e214487c7e877a263
Neuron Grid <neuron-grid@neuron-grid.net>
2025-04-11 01:59:28 +0900

    $domain/api/v1/のようにAPIをバージョン管理するように変更しました。

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    // すべてのルートに/apiプレフィックスを追加
+    app.setGlobalPrefix('api')
+    // バージョニングを有効化
+    // /URIベース /v1 をデフォルトにする
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: '1',
+    })
     await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
